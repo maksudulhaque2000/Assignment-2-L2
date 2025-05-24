247bf5ba4eb62aad0114f6e78423b72fd70a1537
Maksudul-Haque <smmaksudulhaque2000@gmail.com>
2025-05-24 23:43:16 +0600

    Created Tables

---
+++ PostgreSQL_Assignment.sql
@@ -0,0 +1,25 @@
+-- 1. rangers table
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+
+-- 2. species table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) DEFAULT 'Unknown'
+);
+
+-- 3. sightings table
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
+    location VARCHAR(100) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    notes TEXT
+);
