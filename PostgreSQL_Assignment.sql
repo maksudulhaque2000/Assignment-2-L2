f87e42ae3bb466861f667223946076bf7a53e59c
Maksudul-Haque <smmaksudulhaque2000@gmail.com>
2025-05-24 23:46:13 +0600

    Make Ready File

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -46,3 +46,50 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
 
 -- Rangers INSERT
 INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
+
+-- Unique Species Count
+SELECT COUNT(DISTINCT species_id) AS unique_species_count FROM sightings;
+
+-- Finding Pass
+SELECT * FROM sightings WHERE location ILIKE '%Pass%';
+
+-- Every ranger Total Sightings
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY r.name;
+
+-- Not Seen
+SELECT common_name
+FROM species
+WHERE species_id NOT IN (
+    SELECT DISTINCT species_id FROM sightings
+);
+
+-- Last 2 sightings
+SELECT sp.common_name, s.sighting_time, r.name
+FROM sightings s
+JOIN species sp ON s.species_id = sp.species_id
+JOIN rangers r ON s.ranger_id = r.ranger_id
+ORDER BY s.sighting_time DESC
+LIMIT 2;
+
+-- Ago 1800 Years Finding species
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+-- Morning | Afternoon | Evening
+SELECT sighting_id,
+  CASE
+    WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+    WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+    ELSE 'Evening'
+  END AS time_of_day
+FROM sightings;
+
+-- Delete Species
+DELETE FROM rangers
+WHERE ranger_id NOT IN (
+    SELECT DISTINCT ranger_id FROM sightings
+);
